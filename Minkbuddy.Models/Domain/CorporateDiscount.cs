2aa97f352c7f54647d7ddf94abf2662a35e83fe1
Ajay Sharma <ruthik09@gmail.com>
2021-07-22 13:27:15 +0530

    /*Update - Orders

---
+++ Minkbuddy.Models/Domain/CorporateDiscount.cs
@@ -0,0 +1,15 @@
+﻿//Ajay Sharma - 20-07-2021
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minkbuddy.Models.Domain
+{
+    public class CorporateDiscount
+    {
+        public string Label { get; set; }
+        public string Amount { get; set; }
+        public string Percentage { get; set; }
+    }
+}
